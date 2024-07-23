65dce45d1395a308294b2af499e94db2ba824594
Jayeed Bin Hossain <126898481+jayeedwebdevloper@users.noreply.github.com>
2024-07-24 03:39:35 +0600

    Add files via upload

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-slate-200 py-3 text-center px-2'>All right reserve - 2024</div>
   )
 }
